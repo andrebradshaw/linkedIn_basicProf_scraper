ae14d8b027e5e21fa6218b3e51b0d9bb53794db7
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-06 12:27:14 -0500

    Update expandElements.js

--- expandElements.js
+++ expandElements.js
@@ -1,26 +1,46 @@
+function scroller(){
+setTimeout(()=>{
+	window.scrollTo(0,(document.body.scrollHeight - document.body.scrollTop)/2);
+},1);
+setTimeout(()=>{
+	window.scrollTo(0,document.getElementById("experience-section").scrollHeight);
+},500);
+setTimeout(()=>{
+	window.scrollTo(0,document.body.scrollHeight);
+},900);
+setTimeout(()=>{
+	window.scrollTo(0,document.getElementById("skill-categories-expanded").scrollHeight);
+},1200);
+}
+
 function expander(classname){
   setTimeout(()=>{
 	  var exp = document.getElementsByClassName(classname);
 	  for(x=0; x<exp.length; x++){
 		  exp[x].click();
 	  }
-  },800);
+  },2900);
 }
-function expandCondition(classname){
-	for(i=0; i<document.getElementsByClassName(classname).length; i++){
-		var btn = document.getElementsByClassName(classname)[i].getElementsByTagName("button")[0];
-		var btnTxt = btn.innerText;
-		if(/fewer|\bless\b/.test(btnTxt) === false) btn.click();
+
+function infiniteExpand(n){
+setTimeout(()=>{
+var actionBtns = document.getElementsByTagName("button");
+for(a=0; a<actionBtns.length; a++){
+	var btn = actionBtns[a];
+	if(btn != undefined){
+	if(/more(?!…)/i.test(btn.innerText) === true){
+		btn.click();
 	}
+    }
+}
+},((n+1)*366));
+}
+function runFirstExpansion(){
+for(x=0; x<8; x++){
+	infiniteExpand(x);
+}
 }
 
-var dscClassExp = "lt-line-clamp__more";
-var expClassExp = "pv-profile-section__actions-inline ember-view";
-var sumClassExp = "pv-top-card-section__summary-toggle-button pv-profile-section__card-action-bar artdeco-container-card-action-bar mt4";
-var sklClassExp = "pv-profile-section__card-action-bar pv-skills-section__additional-skills artdeco-container-card-action-bar";
-var recClassExp = "pv-profile-section__see-more-inline pv-profile-section__text-truncate-toggle link";
+var scr = new Promise(res =>{	res(scroller());});
 
-
-var expExp = new Promise(res =>{	res(expandCondition(expClassExp));});
-
-expExp.then(expander(dscClassExp))
+scr.then(runFirstExpansion()).then(expander("lt-line-clamp__more"));
