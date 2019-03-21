3c7e4d10592c2982edb0d6c34fddea778cb92d0f
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-03-21 13:49:23 -0700

    Update getLinkedInProfileAsJSON.js

--- getLinkedInProfileAsJSON.js
+++ getLinkedInProfileAsJSON.js
@@ -191,7 +191,7 @@ function getJobExperience() {
         let companyId = vld(/\d+/.exec(workliContainer1.getElementsByTagName("a")[0].href), 0);
         let jobTitle = workliContainer1.getElementsByTagName("h3")[0].innerText.trim();
         let dates = workliContainer1.getElementsByTagName("h4")[1].getElementsByTagName("span")[1].innerText.trim();
-        let geo = workliContainer1.getElementsByTagName("h4")[3].getElementsByTagName("span")[1].innerText.trim();
+        let geo = workliContainer1.getElementsByTagName("h4")[3] ?  workliContainer1.getElementsByTagName("h4")[3].getElementsByTagName("span")[1].innerText.trim() : '';
         let desc = checker(workliContainer1.getElementsByClassName("pv-entity__extra-details"), 0);
         let start = dateParser(vld(rxs.exec(dates), 0));
         let end = dateParser(vld(rxe.exec(dates), 0));
